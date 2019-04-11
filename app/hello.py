98fc561db7a47dfa781c103ab29afe3fa758976f
superwongo <wangchao_198912@126.com>
2019-04-11 18:10:40 +0800

    '测试模板文件使用'

--- app/hello.py
+++ app/hello.py
@@ -6,10 +6,22 @@
 # @File       : hello
 
 from flask.views import View
+from flask import render_template
 
 
 class HelloWorld(View):
     methods = ['GET']
 
     def dispatch_request(self):
-        return 'Hello World!'
+        user = {'nickname': 'Super.Wong'}
+        posts = [
+            {
+                'author': {'nickname': '李白'},
+                'body': '举头望明月，低头思故乡'
+            },
+            {
+                'author': {'nickname': '李清照'},
+                'body': '知否，知否，应是绿肥红瘦'
+            }
+        ]
+        return render_template('hello/hello.html', title='测试', user=user, posts=posts)
