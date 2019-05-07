8e5346cd402a2f86203a18832653e9e47fce8617
superwongo <wangchao_198912@126.com>
2019-05-07 17:07:29 +0800

    邮件发送优化

--- app/email.py
+++ app/email.py
@@ -11,12 +11,10 @@ from flask_mail import Message
 from flask import current_app, render_template
 from flask_babel import _
 
-from app import create_app, mail
+from app import mail
 
 
-def send_async_email(msg):
-    app = create_app()
-    app.app_context().push()
+def send_async_email(app, msg):
     with app.app_context():
         mail.send(msg)
 
@@ -34,7 +32,7 @@ def send_email(subject, sender, recipients, text_body, html_body):
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
-    Thread(target=send_async_email, args=(msg,)).start()
+    Thread(target=send_async_email, args=(current_app._get_current_object(), msg)).start()
 
 
 def send_password_reset_email(user):
