d3da4a0aad2d8636638f1d393b1584ec2cbabc22
superwongo <wangchao_198912@126.com>
2019-05-09 18:00:31 +0800

    新增主应用模块

---
+++ microblog.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project    : flask-mega-tutorial
+# @Author     : wangc
+# @CreateTime : 2019/5/9 17:57
+# @File       : microblog.py
+
+
+from app import create_app, db
+from app.models import User, Post
+
+
+app = create_app()
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post':Post}
